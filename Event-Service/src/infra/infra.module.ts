bd403e230c243e8d3e78f0db9b045b8d588e1b87
gabriel-skinny <gabrielcatoni12@gmail.com>
2024-09-10 16:57:55 -0300

    Adding use cases to module

--- Event-Service/src/infra/infra.module.ts
+++ Event-Service/src/infra/infra.module.ts
@@ -1,10 +1,20 @@
 import { Module } from "@nestjs/common";
-import { GetManyEventsUseCase } from "src/application/use-cases/get-many-events";
-import { OrderTicketUseCaseCase } from "src/application/use-cases/order-ticket";
+
 import { EventController } from "./controllers/event";
 import { DatabaseModule } from "./database/database.module";
 import { ServiceModule } from "./services/services.module";
 import { ClientsModule, Transport } from "@nestjs/microservices";
+import { TicketController } from "./controllers/ticket";
+import { GetManyEventsUseCase } from "src/application/use-cases/event/get-many-events";
+import { OrderTicketsUseCaseCase } from "src/application/use-cases/ticket/order-tickets";
+import { CancelEventUseCase } from "src/application/use-cases/event/cancel-event";
+import { CreateEventUseCase } from "src/application/use-cases/event/create-event";
+import { DeleteEventUseCase } from "src/application/use-cases/event/delete-event";
+import { PublicEventUseCase } from "src/application/use-cases/event/public-event";
+import { UpdateTicketsValueUseCase } from "src/application/use-cases/ticket/update-tickets-value";
+import { GetTicketsByBuyerIdUseCase } from "src/application/use-cases/ticket/get-tickets-by-buyer-id";
+import { MakeTicketAvailableUseCase } from "src/application/use-cases/ticket/make-ticket-available";
+import { BuyTicketUseCase } from "src/application/use-cases/ticket/buy-ticket";
 
 @Module({
   imports: [
@@ -26,7 +36,18 @@ import { ClientsModule, Transport } from "@nestjs/microservices";
     DatabaseModule,
     ServiceModule,
   ],
-  providers: [GetManyEventsUseCase, OrderTicketUseCaseCase],
-  controllers: [EventController],
+  providers: [
+    GetManyEventsUseCase,
+    CancelEventUseCase,
+    CreateEventUseCase,
+    DeleteEventUseCase,
+    PublicEventUseCase,
+    OrderTicketsUseCaseCase,
+    UpdateTicketsValueUseCase,
+    GetTicketsByBuyerIdUseCase,
+    MakeTicketAvailableUseCase,
+    BuyTicketUseCase,
+  ],
+  controllers: [EventController, TicketController],
 })
 export class InfraModule {}
